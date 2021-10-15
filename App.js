46abf2d847c9b9efffca8958c7159275a2ac1f3e
gineff <canone@inbox.ru>
2021-10-15 08:24:13 +0300

    6

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import BookList  from "./Components/BookList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container2}>
+      <BookList/>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,4 +19,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  container2: {
+    width: '100%',
+    padding: 16,
+    paddingTop: 100,
+  }
 });
