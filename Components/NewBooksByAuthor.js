1f58caa0c8ea6e46bc05d72444217806b44a9729
gineff <canone@inbox.ru>
2021-10-19 20:09:26 +0300

    удалены ненужные файлы

--- Components/NewBooksByAuthor.js
+++
@@ -1,8 +0,0 @@
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-export default ({navigation})=> {
-    return <View>
-        <Text>Новые книги по автору</Text>
-        <Button onPress={()=>{navigation.navigate("Feed")}} title="Go back"/>
-    </View>
-}
